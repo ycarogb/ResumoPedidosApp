d73341aa2f027393320ddf3472f1c2911ebe5207
Ycaro Batalha <ycarobatalha1@gmail.com>
2024-05-12 13:25:36 -0300

    Remove projeto de testes

--- Program.cs
+++ Program.cs
@@ -3,39 +3,47 @@ using ResumoPedidos.Data;
 using ResumoPedidos.Data.Repositories;
 using ResumoPedidos.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace ResumoPedidos;
 
-builder.Services.AddScoped<IClienteService, ClienteService>();
-builder.Services.AddScoped<IProdutoService, ProdutoService>();
-builder.Services.AddScoped<IResumoPedidoService, ResumoPedidoService>();
-builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
-builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-builder.Services.AddScoped<IResumoPedidoRepository, ResumoPedidoRepository>();
-builder.Services.AddScoped<ResumoPedidosContext>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(p =>
+public class Program
 {
-    p.SwaggerDoc("v1", new OpenApiInfo() { Title = "ResumoPedidosApi", Version = "v1" });
-    p.ResolveConflictingActions(q => q.First());
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        builder.Services.AddScoped<IClienteService, ClienteService>();
+        builder.Services.AddScoped<IProdutoService, ProdutoService>();
+        builder.Services.AddScoped<IResumoPedidoService, ResumoPedidoService>();
+        builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+        builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        builder.Services.AddScoped<IResumoPedidoRepository, ResumoPedidoRepository>();
+        builder.Services.AddScoped<ResumoPedidosContext>();
 
-SeedDataBase.Initialize(app);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(p =>
+        {
+            p.SwaggerDoc("v1", new OpenApiInfo() { Title = "ResumoPedidosApi", Version = "v1" });
+            p.ResolveConflictingActions(q => q.First());
+        });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        var app = builder.Build();
+
+        SeedDataBase.Initialize(app);
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
