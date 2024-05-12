aa31032e7c2a1d5d71fa6592d346baf62f915af5
Ycaro Batalha <ycarobatalha1@gmail.com>
2024-05-12 13:33:22 -0300

    Apaga pasta de projeto de testes

--- Program.cs
+++ Program.cs
@@ -3,47 +3,40 @@ using ResumoPedidos.Data;
 using ResumoPedidos.Data.Repositories;
 using ResumoPedidos.Services;
 
-namespace ResumoPedidos;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
+builder.Services.AddScoped<IResumoPedidoService, ResumoPedidoService>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<IResumoPedidoRepository, ResumoPedidoRepository>();
+builder.Services.AddScoped<ResumoPedidosContext>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(p =>
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    p.SwaggerDoc("v1", new OpenApiInfo() { Title = "ResumoPedidosApi", Version = "v1" });
+    p.ResolveConflictingActions(q => q.First());
+});
 
-        builder.Services.AddScoped<IClienteService, ClienteService>();
-        builder.Services.AddScoped<IProdutoService, ProdutoService>();
-        builder.Services.AddScoped<IResumoPedidoService, ResumoPedidoService>();
-        builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
-        builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-        builder.Services.AddScoped<IResumoPedidoRepository, ResumoPedidoRepository>();
-        builder.Services.AddScoped<ResumoPedidosContext>();
+var app = builder.Build();
 
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(p =>
-        {
-            p.SwaggerDoc("v1", new OpenApiInfo() { Title = "ResumoPedidosApi", Version = "v1" });
-            p.ResolveConflictingActions(q => q.First());
-        });
+SeedDataBase.Initialize(app);
 
-        var app = builder.Build();
-
-        SeedDataBase.Initialize(app);
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
